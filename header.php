0175ba2f3443e7bfa659b2143691c44a4f6229e8
channy2788 <hello@chantelesmith.co.uk>
2015-01-12 15:32:32 +0000

    header
    file updates

--- header.php
+++ header.php
@@ -1,2 +1,23 @@
-<?php get_header(); ?>
-<div id="wrapper">
+<!DOCTYPE HTML>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo('charset');?>">
+		<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' />
+		
+		<title>
+			<?php
+				global $page, $paged;
+				wp_title( '|', true, 'right' );
+				bloginfo( 'name' );
+				$site_description = get_bloginfo( 'description', 'display' );
+				if ( $site_description && ( is_home() || is_front_page() ) )
+					echo " | $site_description";
+				if ( $paged >= 2 || $page >= 2 )
+					echo ' | ' . sprintf( __( 'Page %s' ), max( $paged, $page ) );
+			?>
+		</title>
+
+		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+		<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,700,300,200' rel='stylesheet' type='text/css'>
+
+		<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script('comment-reply'); ?>
